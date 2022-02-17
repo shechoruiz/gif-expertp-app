857eaf9756d5d2e9c28327de3e737afce54b627f
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-16 23:06:49 -0500

    Creación componente GitExpertApp

---
+++ src/GifExpertApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+const GifExpertApp = () => {
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <hr />
+    </>
+  );
+};
+
+// GifExpertApp.propTypes = {};
+
+export default GifExpertApp;
