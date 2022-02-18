4c09ed435d6fbcc78067ff8e57e9d8fc9b1b3f4b
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-17 22:56:56 -0500

    Mostrar resultados de busqueda

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const AddCategory = ({ setCategories }) => {
-  const [inputValue, setInputValue] = useState("Hola mundo");
+  const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import GifGridItem from "./GifGridItem";
 
 const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    getGifs();
+  }, []);
+
   const getGifs = async () => {
     const url =
-      "https://api.giphy.com/v1/gifs/search?q=Goku&limit=5&api_key=kx88TEZM2i1M6R4HS35zxEarXqOLCjz1";
+      "https://api.giphy.com/v1/gifs/search?q=Goku&limit=10&api_key=kx88TEZM2i1M6R4HS35zxEarXqOLCjz1";
     const res = await fetch(url);
     const { data } = await res.json();
 
@@ -17,13 +24,15 @@ const GifGrid = ({ category }) => {
     });
 
     console.log(gifs);
+    setImages(gifs);
   };
-
-  getGifs();
 
   return (
     <div>
       <h3>{category}</h3>
+      {images.map((img) => (
+        <GifGridItem key={img.id} {...img} />
+      ))}
     </div>
   );
 };
