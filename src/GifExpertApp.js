27bef935e7a2853e403cd073e7274ee651318e59
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-17 22:07:17 -0500

    Creación componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import AddCategory from "./components/AddCategory";
 // import PropTypes from "prop-types";
 
 const GifExpertApp = () => {
+  // const categories = ["One Punch", "Samurai X", "Dragon Ball Z"];
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball Z",
+  ]);
+
+  // const handleAdd = () => {
+  //   setCategories([...categories, "Ranma 1/2"]);
+  //   // La siguiente línea tambien permite actualizar el state con un callback
+  //   setCategories((cat) => [...cat, "Hunter X Hunter"]);
+  // };
+
   return (
     <>
       <h2>GifExpertApp</h2>
+      <AddCategory />
       <hr />
+      <ol>
+        {categories.map((value, id) => (
+          <li key={id}>{value}</li>
+        ))}
+      </ol>
     </>
   );
 };
---
+++ src/components/AddCategory.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const AddCategory = () => {
+  const [inputValue, setInputValue] = useState("Hola mundo");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </form>
+  );
+};
+
+export default AddCategory;
