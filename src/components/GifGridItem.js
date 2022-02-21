448125c53f28d68a4bccab91eb2eb5d203248b87
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-20 22:50:25 -0500

    Prueba inicial de proptypes en componente

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Proptypes from "prop-types";
 
 const GifGridItem = ({ title, url }) => {
   return (
@@ -7,6 +8,11 @@ const GifGridItem = ({ title, url }) => {
       <p>{title}</p>
     </div>
   );
+};
+
+GifGridItem.propTypes = {
+  title: Proptypes.string.isRequired,
+  url: Proptypes.string.isRequired,
 };
 
 export default GifGridItem;
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,8 +4,10 @@ import { shallow } from "enzyme";
 import GifGridItem from "../../components/GifGridItem";
 
 describe("Pruebas sobre el componente GifGridItem", () => {
+  const titulo = "Un título";
+  const url = "https://localhost/algo.jpg";
   test("Debe mostrar el componente correctamente", () => {
-    const wrapper = shallow(<GifGridItem />);
+    const wrapper = shallow(<GifGridItem title={titulo} url={url} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
