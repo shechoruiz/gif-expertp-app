bdc579afe26d95915c0e23fba6e651d162c43d18
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-20 21:39:17 -0500

    Implementación de animaciones sencillas

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -9,9 +9,7 @@ const useFetchGifs = (category) => {
 
   useEffect(() => {
     getGifs(category).then((imgs) => {
-      setTimeout(() => {
-        setState({ data: imgs, loading: false });
-      }, 3000);
+      setState({ data: imgs, loading: false });
     });
   }, [category]);
 
