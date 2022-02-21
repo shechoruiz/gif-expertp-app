8fdb10efa4a9ea6da71c2a3d17416f5f69ec5bc0
Sergio Andrés Ruiz Vélez <shecho27@gmail.com>
2022-02-20 22:37:47 -0500

    Configuración del ambiente de pruebas

---
+++ src/tests/GifGridItem.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { shallow } from "enzyme";
+import GifGridItem from "../components/GifGridItem";
+
+describe("Pruebas sobre el componente GifGridItem", () => {
+  test("Debe mostrar el componente correctamente", () => {
+    const wrapper = shallow(<GifGridItem />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
